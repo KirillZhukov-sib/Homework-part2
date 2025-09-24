a55ccf57453aca95c12efe4d4693d7d2a02a6a5a
Кирилл Жуков <kirillsiber@yandex.ru>
2025-09-24 22:37:05 +0700

    Lesson 2

--- Lesson 2.1/MainWindow.xaml.cs
+++ Lesson 2.1/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Lesson_2._1
 {
@@ -19,6 +21,33 @@ namespace Lesson_2._1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextBox1.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, TextBox1.Text);
+            }
+        }
+
+        private void Open2_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 about = new Window1();
+            about.Owner = this;
+            about.ShowDialog();
         }
     }
 }
