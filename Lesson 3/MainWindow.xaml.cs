f0815eb575459fad9521065d50d4bfa59908e11e
Кирилл Жуков <kirillsiber@yandex.ru>
2025-10-02 16:17:24 +0700

    Lesson 3

--- Lesson 3/MainWindow.xaml.cs
+++ Lesson 3/MainWindow.xaml.cs
@@ -20,5 +20,61 @@ namespace Lesson_3
         {
             InitializeComponent();
         }
+
+        
+        private void FacultyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FacultyComboBox.SelectedItem == null) return;
+
+            string selectedFaculty = (FacultyComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+            TopicsListBox.Items.Clear();
+            switch (selectedFaculty)
+            {
+                case "C#":
+                    TopicsListBox.Items.Add("Графика");
+                    TopicsListBox.Items.Add("Операторы");
+                    TopicsListBox.Items.Add("Операции");
+                    TopicsListBox.Items.Add("Многопоточность");
+                    TopicsListBox.Items.Add("Asynk/Await");
+                    break;
+
+                case "Python":
+                    TopicsListBox.Items.Add("Веб-разработка");
+                    TopicsListBox.Items.Add("Data Science");
+                    TopicsListBox.Items.Add("Машинное обучение");
+                    TopicsListBox.Items.Add("Django");
+                    TopicsListBox.Items.Add("NumPy");
+                    break;
+
+                case "Java":
+                    TopicsListBox.Items.Add("Spring Framework");
+                    TopicsListBox.Items.Add("Android разработка");
+                    TopicsListBox.Items.Add("Hibernate");
+                    TopicsListBox.Items.Add("Maven");
+                    TopicsListBox.Items.Add("JUnit");
+                    break;
+            }
+        }
+
+        private void record_Click(object sender, RoutedEventArgs e)
+        {
+            if (!check.IsChecked ?? false)
+            {
+                MessageBox.Show("Ошибка: необходимо согласиться на обработку персональных данных!", "Ошибка",
+                                MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            var selectedCourses = TopicsListBox.SelectedItems
+            .Cast<string>()
+            .ToList();
+
+            string message = $"Профиль сохранен!\n\n" +
+                             $"Имя: {TextName.Text}\n" +
+                             $"Факультет: {FacultyComboBox.Text}\n" +
+                             $"Выбранные курсы: {selectedCourses}\n" +
+                             $"Согласие на рассылку: {(check.IsChecked == true ? "Да" : "Нет")}";
+
+            MessageBox.Show(message, "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
