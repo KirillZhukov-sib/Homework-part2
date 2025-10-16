14805ddc8faee2c76c779449ea12d627c60206cc
Кирилл Жуков <kirillsiber@yandex.ru>
2025-10-16 23:48:01 +0700

    Lesson 9

---
+++ Lesson 9/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Lesson_9
+{
+    internal class CustomCommands
+    {
+        public static RoutedUICommand ChangeColor { get; set; }
+        static CustomCommands()
+        {
+            InputGestureCollection inputGestureCollection = new InputGestureCollection();
+            inputGestureCollection.Add(new KeyGesture(Key.C, ModifierKeys.Control, "Ctrl+C"));
+
+            ChangeColor = new RoutedUICommand("Change Color", "ChangeColor", typeof(CustomCommands), inputGestureCollection);
+        }
+    }
+}
---
+++ Lesson 9/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Stack<Brush> _colorHistory = new Stack<Brush>();
+        private Brush _currentColor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CurrentColor = Brushes.White; // Инициализация начального цвета
+        }
+
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var random = new Random();
+            CurrentColor = new SolidColorBrush(Color.FromRgb(
+                (byte)random.Next(256),
+                (byte)random.Next(256),
+                (byte)random.Next(256)));
+        }
+
+        public Brush CurrentColor
+        {
+            get => _currentColor;
+            set
+            {
+                _currentColor = value;
+                if (_colorHistory.Count == 0 || _colorHistory.Peek() != value)
+                {
+                    _colorHistory.Push(value);
+                }
+                Dock.Background = _currentColor;
+            }
+        }
+
+        private void UndoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_colorHistory.Count > 1)
+            {
+                _colorHistory.Pop(); // Удаляем текущий цвет
+                CurrentColor = _colorHistory.Peek(); // Берем предыдущий
+            }
+        }
+
+        private void UndoCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _colorHistory.Count > 1;
+        }
+    }
+}
